771e209152d3f6937969a3075e0053b0dda14722
agape1225 <scg9268@gmail.com>
2023-09-12 18:24:19 +0900

    init

---
+++ main.py
@@ -0,0 +1,110 @@
+from flask import Flask, request, render_template
+from bs4 import BeautifulSoup
+import urllib
+import urllib.request
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from system.AI.lstm_machine import LstmMachine
+
+app = Flask(__name__)
+
+
+@app.route("/test_model")
+def model():
+    #new_model = tf.keras.models.load_model('static/BITCOIN_MODEL_VER1')
+
+    """loaded_model = load_model("static/BITCOIN_MODEL_VER1.h5", compile=False)
+    predicted_values = [
+        [35268000.0],
+        [35616000.0],
+        [34978000.0],
+        [35019000.0],
+        [34951000.0],
+        [35246000.0],
+        [35142000.0],
+        [35319000.0],
+        [35442000.0],
+        [37023000.0],
+        [37354000.0],
+        [35268000.0],
+        [35355000.0],
+        [35351000.0],
+        [35425000.0]]
+    predicted_values.reverse()
+    scaler=MinMaxScaler(feature_range=(0,1))
+    predicted_values = scaler.fit_transform(
+    np.array(predicted_values).reshape(-1, 1))
+    predicted_values = np.array([predicted_values])
+
+    predicted = loaded_model.predict(predicted_values)
+    predicted_values.shape, predicted.shape
+
+    print(predicted_values)
+
+    predicted = scaler.inverse_transform(predicted)
+    print(predicted)"""
+
+    test_value = [
+        35268000.0,
+        35616000.0,
+        34978000.0,
+        35019000.0,
+        34951000.0,
+        35246000.0,
+        35142000.0,
+        35319000.0,
+        35442000.0,
+        37023000.0,
+        37354000.0,
+        35268000.0,
+        35355000.0,
+        35351000.0,
+        35425000.0
+    ]
+
+    test_value.reverse()
+
+    aiMachine = LstmMachine()
+
+    data = aiMachine.data_processing(test_value)
+    predicted_data = aiMachine.get_predict_value(data)
+
+    return "예측값: " + str(predicted_data)  
+
+
+
+
+@app.route("/")
+def home():
+    html = """
+    <html><head><meta charset="utf-8"></head>
+  <body>
+     날씨정보<br/>
+    <form action = "/weather">
+      <input type = "text" name = "city" />
+      <input type = "submit"/>
+    </form>
+  </body>
+</html>
+"""
+    return html
+
+
+@app.route('/weather')
+def weather():
+    city = request.args.get("city", "")
+    url = "https://search.naver.com/search.naver?&query="
+    url = url + urllib.parse.quote_plus(city + "날씨")
+
+    soup = BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")
+
+    temp = soup.select("div.weather_graphic > div.temperature_text > strong")
+    desc = soup.select("div._today > div.temperature_info > p")
+    summary = soup.select("div._today > div.temperature_info > dl")
+
+    return render_template("weather.html", weather={"city": city, "temp": temp[0].text, "desc": desc[0].text, "summary": summary[0].text})
+
+
+app.run(debug=True)
